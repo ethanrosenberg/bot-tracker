5b0fbc1b92cad40530e5ca8a9bad72f710386683
Ethan Rosenberg <ethan@ethanrosenberg.com>
2020-03-13 21:13:20 -0700

    Added Search model, controller and resources route.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_14_040854) do
+ActiveRecord::Schema.define(version: 2020_03_14_041252) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 2020_03_14_040854) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.string "keyword"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
