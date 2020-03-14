4636e12653ff37ed4339a854348a3ef3b29bfe3d
Ethan Rosenberg <ethan@ethanrosenberg.com>
2020-03-14 00:07:44 -0700

    Created xpath parsing with nokogiri for each individual tweet with relevant data.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,16 +7,23 @@ class SearchesController < ApplicationController
 
   def start
 
-byebug
+
     task = crawl_url("")
 
-    html =  File.read('sample.html')
-    nok = Nokogiri::HTML.parse(html)
+    #html =  File.read('sample.html')
+    nok = Nokogiri::HTML.parse(task)
     #nok.xpath("//table[@class='tweet-container']")
 
     nok.xpath("//table[@class='tweet  \']").each do |tweet|
-      #<tr class=\"tweet-container\">\n
-      byebug
+      #<tr class=\"tweet-container\">\# NOTE:
+      #tweet_text = tweet.text.strip
+      tweet_url = "https://twitter.com/" + tweet.attr('href')
+      profile_pic = tweet.xpath(".//img").attr('src').value
+      handle = tweet.xpath(".//td[@class='user-info\']//a").attr('href').value.chomp("?p=s")
+      tweet_content = tweet.xpath(".//tr[@class='tweet-container\']").text.strip
+      puts "Handle - " + handle.chomp("?p=s")
+      puts "Tweet >> " + tweet_content
+      puts " "
 
     end
 
@@ -38,9 +45,10 @@ byebug
 
 #https://twitter.com/search?q=americanbulldog%20since%3A2015-05-01%20until%3A2015-05-05
 
-    return open(url,
-            "User-Agent"=> 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:9.0.1) Gecko/20100101 Firefox/9.0.1',
-            :proxy => "http://69.46.80.226:19403").read.to_s
+    return open("https://twitter.com/search?q=americanbulldog%20since%3A2015-05-01%20until%3A2015-05-05",
+            "User-Agent" => 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:9.0.1) Gecko/20100101 Firefox/9.0.1').read.to_s
+
+            #:proxy => "http://69.46.80.226:19403").read.to_s
 
     #open("https://twitter.com/search?q=americanbulldog%20since%3A2015-05-01%20until%3A2015-05-05",
             # {"User-Agent" => AGENT,
