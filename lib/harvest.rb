435ce24db39be721c4bd2b6facec405759ce75fc
Ethan Rosenberg <ethan@ethanrosenberg.com>
2020-03-18 23:43:25 -0700

    removed extra class name

--- lib/harvest.rb
+++ lib/harvest.rb
@@ -2,7 +2,7 @@
 
 module Harvest
 
-  @@client = @client = Twitter::REST::Client.new do |config|
+  @@client = Twitter::REST::Client.new do |config|
     config.consumer_key        = ENV["CONSUMER_KEY"]
     config.consumer_secret     = ENV["CONSUMER_SECRET"]
     config.access_token        = ENV["ACCESS_TOKEN"]
