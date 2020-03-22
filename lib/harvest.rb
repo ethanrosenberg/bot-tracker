38b72ac3509ef06aaaac14e2765194844252cd06
Ethan Rosenberg <ethan@ethanrosenberg.com>
2020-03-22 01:44:54 -0700

    Changed harvest vals for testing.

--- lib/harvest.rb
+++ lib/harvest.rb
@@ -321,7 +321,7 @@ module Harvest
 
   def update_progress
     percent_finished_string = get_percentage_done()
-    ActionCable.server.broadcast 'web_notifications_channel', id: @search_id, message: @percent_finished
+    ActionCable.server.broadcast 'web_notifications_channel', id: search_id, message: 33
   end
 
 
