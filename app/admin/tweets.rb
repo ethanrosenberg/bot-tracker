67ecd57171a7d9a3f7f11345034bcef761d18cb8
Ethan Rosenberg <ethan@ethanrosenberg.com>
2020-03-23 14:22:07 -0700

    Customized tweets view

--- app/admin/tweets.rb
+++ app/admin/tweets.rb
@@ -1,5 +1,18 @@
 ActiveAdmin.register Tweet do
 menu priority: 4
+
+index do
+  column "id", :id
+  column "Tweet Id", :tweet_id
+  column "Text", :text
+  column "Handle", :profile_handle
+  column "Profile Created At", :profile_created_at
+  column "Followers", :followers
+  column "Default Profile Pic?", :default_profile_pic
+  column "Created At", :created_at
+end
+
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
