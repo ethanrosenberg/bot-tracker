a4860c779a954205f4a13f567f5e5fd642a11344
Ethan Rosenberg <ethan@ethanrosenberg.com>
2020-03-23 02:00:18 -0700

    Removed Comments from main menu

--- app/admin/settings.rb
+++ app/admin/settings.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Setting do
-
+menu label: "My Posts"
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
